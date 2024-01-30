df9d9315d717f8046253ea17a68d01c2e57e5c52
ShamailAbbas <shamailabbas61@gmail.com>
2024-01-30 15:33:43 +0500

    fixed blink

--- pages/[state]/[city]/index.js
+++ pages/[state]/[city]/index.js
@@ -1,233 +1,3 @@
-// import ArticlePreview from "@/components/CityPage/ArticlePreview";
-// import Attorny from "@/components/CityPage/Attorny";
-// import Faq from "@/components/CityPage/Faq";
-// import Header from "@/components/CityPage/Header";
-// import Review from "@/components/CityPage/Review";
-// import YoutubeEmbed from "@/components/CityPage/YoutubeEmbed";
-// import ReviewForm from "@/components/CityPage/ReviewForm";
-// import { useRouter } from "next/router";
-// import React, { useEffect, useState } from "react";
-// import { header } from "@/data";
-// import deslugify from "@/utils/deslugify";
-// import { fetchArticleByCity } from "@/lib/article";
-// import { addAttorney, fetchAttorneyByCity } from "@/lib/attorny";
-// import { addVideo, fetchVideosByCity } from "@/lib/video";
-// import {
-//   addReview,
-//   fetchAverageStarsByCity,
-//   fetchReviewByCity,
-// } from "@/lib/review";
-// import isSubdomainAdmin from "@/utils/isSubdomainAdmin ";
-// import VideoForm from "@/components/CityPage/VideoForm";
-// import AttorneyForm from "@/components/CityPage/AttornyForm";
-// import { getFaqByCity } from "@/lib/faq";
-// import { GetCityByName } from "@/lib/city";
-
-// const City = ({
-//   attorney,
-//   articles,
-//   videoIds,
-//   reviews,
-//   averageStars,
-//   isAdmin,
-//   faqs,
-//   cityFound,
-// }) => {
-//   const router = useRouter();
-//   const { state, city, article } = router.query;
-//   const [_isAdmin, set_IsAdmin] = useState(false);
-//   const [showForm, setShowForm] = useState("");
-//   useEffect(() => {
-//     set_IsAdmin(isAdmin);
-//   }, []);
-//   function renderForm() {
-//     if (showForm == "review")
-//       return (
-//         <ReviewForm
-//           onSubmit={async (data) => {
-//             await addReview(data);
-//             setShowForm("");
-//           }}
-//           setShowForm={setShowForm}
-//           city={city && deslugify(city)}
-//         />
-//       );
-//     if (showForm == "video")
-//       return (
-//         <VideoForm
-//           onSubmit={async (data) => {
-//             await addVideo(data);
-//             setShowForm("");
-//           }}
-//           setShowForm={setShowForm}
-//           city={city && deslugify(city)}
-//         />
-//       );
-//     if (showForm == "attorny")
-//       return (
-//         <AttorneyForm
-//           onSubmit={async (data) => {
-//             await addAttorney(data);
-//             setShowForm("");
-//           }}
-//           setShowForm={setShowForm}
-//           city={city && deslugify(city)}
-//           state={state && deslugify(state)}
-//         />
-//       );
-//   }
-//   if (!cityFound) {
-//     return (
-//       <p className="w-full h-screen flex items-center justify-center font-bold text-40 ">
-//         City not found
-//       </p>
-//     );
-//   }
-//   return (
-//     <div className="flex flex-col w-full  items-center font-Poppins px-4">
-//       <div className="flex flex-col justify-center items-center sm:max-w-[500px] w-full">
-//         <Header
-//           header={header}
-//           city={city && deslugify(city)}
-//           state={state && deslugify(state)}
-//         />
-
-//         {isAdmin && (
-//           <button
-//             className="flex items-center w-full justify-center py-2 my-4 rounded-full border-[1.5px] border-red-300 hover:bg-red-200 bg-red-100 text-red-800 text-16 font-bold "
-//             onClick={() => setShowForm("attorny")}
-//           >
-//             Add A New Attorny
-//           </button>
-//         )}
-//         {attorney.map((data, index) => (
-//           <Attorny data={data} key={index} />
-//         ))}
-//         {isAdmin && (
-//           <button
-//             className="flex items-center w-full justify-center py-2 my-4 rounded-full border-[1.5px] border-red-300 hover:bg-red-200 bg-red-100 text-red-800 text-16 font-bold "
-//             onClick={() => setShowForm("review")}
-//           >
-//             Add A New Review
-//           </button>
-//         )}
-//         {reviews?.reviews?.length > 0 && (
-//           <Review averageStars={averageStars} reviews={reviews} />
-//         )}
-//         {isAdmin && (
-//           <button
-//             className="flex items-center w-full justify-center py-2 my-4 rounded-full border-[1.5px] border-red-300 hover:bg-red-200 bg-red-100 text-red-800 text-16 font-bold "
-//             onClick={() => setShowForm("faqs")}
-//           >
-//             Generate Faqs
-//           </button>
-//         )}
-//         {faqs?.length > 0 && <Faq faqs={faqs} />}
-//         {videoIds.length > 0 && (
-//           <p className=" self-start uppercase text-[22px]  opacity-90 mt-8">
-//             PERSONAL INJURY VIDEOS FOR {deslugify(city)}
-//           </p>
-//         )}
-//         {isAdmin && (
-//           <button
-//             className="flex items-center w-full justify-center py-2 my-4 rounded-full border-[1.5px] border-red-300 hover:bg-red-200 bg-red-100 text-red-800 text-16 font-bold "
-//             onClick={() => setShowForm("video")}
-//           >
-//             Add A New Video
-//           </button>
-//         )}
-//         {videoIds.map((i, index) => (
-//           <YoutubeEmbed videoId={i.videoId} key={index} />
-//         ))}
-//         {articles.length > 0 && (
-//           <p className=" self-start  uppercase text-[22px]   opacity-90 mt-8">
-//             ARTICLES
-//           </p>
-//         )}
-//         {isAdmin && (
-//           <button className="flex items-center w-full justify-center py-2 my-4 rounded-full border-[1.5px] border-red-300 hover:bg-red-200 bg-red-100 text-red-800 text-16 font-bold ">
-//             Add A New Article
-//           </button>
-//         )}
-//         {articles.map((article, index) => (
-//           <ArticlePreview
-//             key={index}
-//             imageUrl={article.imageUrl}
-//             title={article.title}
-//             date={article.date}
-//             description={article.description}
-//             type={article.type}
-//             state={article.state}
-//             city={article.city}
-//             slug={article.slug}
-//           />
-//         ))}
-//         {renderForm()}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default City;
-
-// export const getServerSideProps = async ({ params, req }) => {
-//   let isAdmin = false;
-//   let cityFound = false;
-//   try {
-//     // Extract subdomain from the hostname
-//     const subdomain = req.headers.host.split(".")[0];
-
-//     if (subdomain.toLowerCase() === "admin") {
-//       isAdmin = true;
-//     }
-//     const { city, state } = params;
-//     const city_name = deslugify(city);
-//     const state_name = deslugify(state);
-
-//     const _city = await GetCityByName(city_name, state_name);
-//     // console.log("_city", _city);
-
-//     if (!_city._id) {
-//       throw new Error();
-//     }
-
-//     const articles = await fetchArticleByCity(city_name);
-
-//     const attorney = await fetchAttorneyByCity(city_name);
-
-//     const videoIds = await fetchVideosByCity(city_name);
-//     const reviews = await fetchReviewByCity(city_name);
-//     const averageStars = await fetchAverageStarsByCity(city_name);
-//     const faqs = await getFaqByCity(city_name);
-//     // console.log("articles", articles);
-//     return {
-//       props: {
-//         attorney,
-//         articles,
-//         videoIds,
-//         reviews,
-//         averageStars: averageStars?.averageStars || null,
-//         isAdmin,
-//         faqs,
-//         cityFound: true,
-//       },
-//     };
-//   } catch (error) {
-//     return {
-//       props: {
-//         attorney: [],
-//         articles: [],
-//         videoIds: [],
-//         reviews: {},
-//         averageStars: null,
-//         isAdmin,
-//         faqs: [],
-//         cityFound,
-//       },
-//     };
-//   }
-// };
-
 import ArticlePreview from "@/components/CityPage/ArticlePreview";
 import Attorny from "@/components/CityPage/Attorny";
 import Faq from "@/components/CityPage/Faq";
@@ -271,10 +41,9 @@ const City = ({
   const { state, city } = router.query;
   const [_isAdmin, set_IsAdmin] = useState(false);
   const [showForm, setShowForm] = useState("");
-  const [showArticleForm, setshowArticleForm] = useState(true);
+
   useEffect(() => {
     set_IsAdmin(isAdmin);
-    setshowArticleForm("");
   }, []);
 
   function showMessage(message, closetime = 1000) {
@@ -369,7 +138,7 @@ const City = ({
         {isAdmin && (
           <div
             className="flex items-center w-full justify-center py-2 my-4 rounded-full border-[1.5px] border-red-300 hover:bg-red-200 bg-red-100 text-red-800 text-16 font-bold cursor-pointer"
-            onClick={() => setshowArticleForm(true)}
+            onClick={() => setShowForm(4)}
           >
             Add A New Article
           </div>
@@ -429,7 +198,7 @@ const City = ({
           />
         )}
 
-        {showArticleForm && (
+        {showForm == 4 && (
           <ArticleForm
             onSubmit={async (data) => {
               console.log("article data is ", data);
@@ -439,7 +208,7 @@ const City = ({
                 setShowForm("");
               }
             }}
-            setShowForm={setshowArticleForm}
+            setShowForm={setShowForm}
             city={city && deslugify(city)}
             state={state && deslugify(state)}
           />
