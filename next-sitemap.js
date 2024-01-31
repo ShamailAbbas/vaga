6da79d1826caab96f36d6d6085bdaaea1b5a1342
ShamailAbbas <shamailabbas61@gmail.com>
2024-01-31 14:29:26 +0500

    added site map

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: "https://personalinjurynearme.attorney", // Replace with your actual website URL
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  // Add other configurations as needed
+};
