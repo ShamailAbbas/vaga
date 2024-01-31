aa028a570a5aaf52e14d2aa5aec0e7348870c2cb
model.continet28@gmail.com <shamailabbas61@gmail.com>
2024-01-31 17:57:32 +0500

    added cities

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -5,24 +5,21 @@ import { getServerSideSitemapLegacy } from "next-sitemap";
 
 export const getServerSideProps = async (ctx) => {
   const articles = await fetchArticles();
-  const AllArticle = articles?.map((i) => {
-    return {
+  const AllArticle = articles?.map((i) => ({    
       loc: `https://personalinjurynearme.attorney/${slugify(i.state)}/${slugify(
         i.city
       )}/${i.slug}`,
       lastmod: new Date(i?.date),
-    };
-  });
-
+    
+  }));
+console.log(AllArticle)
   const cities = await getAllCities();
-  const AllCities = cities?.map((i) => {
-    return {
-      loc: `https://personalinjurynearme.attorney/${slugify(
-        i.state_name
-      )}/${slugify(i.city)}`,
-      lastmod: new Date(),
-    };
-  });
+  const AllCities = cities?.map((i) => ({
+    loc: `https://personalinjurynearme.attorney/${slugify(
+      i.state_name
+    )}/${slugify(i.city)}`,
+    lastmod: new Date(),
+  }));
   const fields=[...AllArticle,...AllCities]
   return getServerSideSitemapLegacy(ctx, fields);
 };
