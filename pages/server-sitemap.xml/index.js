13daa9726f3894b649f1e3f9d895b90f9c41240e
model.continet28@gmail.com <shamailabbas61@gmail.com>
2024-01-31 16:25:57 +0500

    add site maps

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -1,10 +1,11 @@
 import { fetchArticles } from "@/lib/article";
+import { getAllCities } from "@/lib/city";
 import slugify from "@/utils/slugify";
 import { getServerSideSitemapLegacy } from "next-sitemap";
 
 export const getServerSideProps = async (ctx) => {
   const articles = await fetchArticles();
-  const fields = articles?.map((i) => {
+  const AllArticle = articles?.map((i) => {
     return {
       loc: `https://personalinjurynearme.attorney/${slugify(i.state)}/${slugify(
         i.city
@@ -12,6 +13,17 @@ export const getServerSideProps = async (ctx) => {
       lastmod: new Date(i?.date),
     };
   });
+
+  const cities = await getAllCities();
+  const AllCities = cities?.map((i) => {
+    return {
+      loc: `https://personalinjurynearme.attorney/${slugify(
+        i.state_name
+      )}/${slugify(i.city)}`,
+      lastmod: new Date(),
+    };
+  });
+  const fields=[...AllArticle,...AllCities]
   return getServerSideSitemapLegacy(ctx, fields);
 };
 
